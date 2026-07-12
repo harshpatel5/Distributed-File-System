3806f598a673ce4c509056f900cf8f03fb34e45e
PatH5 <43816130+harshpatel5@users.noreply.github.com>
2026-07-11 22:32:29 -0400

    Added end-to-end tests

---
+++ dfs/tests/e2e/e2e_test.go
@@ -0,0 +1,138 @@
+package e2e
+
+import (
+	"io"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"distributed-file-system/internal/config"
+	"distributed-file-system/internal/storage/chunk"
+	"distributed-file-system/pkg/logging"
+	"distributed-file-system/pkg/utils"
+)
+
+func compareChecksums(t *testing.T, originalFilePath string, downloadedFilePath string, testName string) {
+	originalFile, err := os.Open(originalFilePath)
+	if err != nil {
+		t.Fatalf("%s: failed to open file: %v", testName, err)
+	}
+	defer originalFile.Close()
+	originalFileData, err := io.ReadAll(originalFile)
+	if err != nil {
+		t.Fatalf("%s: failed to read file: %v", testName, err)
+	}
+	originalFileChecksum := chunk.CalculateChecksum(originalFileData)
+	t.Logf("%s: Original file checksum: %s", testName, originalFileChecksum)
+
+	downloadedFile, err := os.Open(downloadedFilePath)
+	if err != nil {
+		t.Fatalf("%s: failed to open downloaded file: %v", testName, err)
+	}
+	defer downloadedFile.Close()
+	downloadedFileData, err := io.ReadAll(downloadedFile)
+	if err != nil {
+		t.Fatalf("%s: failed to read file: %v", testName, err)
+	}
+	downloadedFileChecksum := chunk.CalculateChecksum(downloadedFileData)
+	t.Logf("%s: Downloaded file checksum: %s", testName, downloadedFileChecksum)
+
+	if originalFileChecksum != downloadedFileChecksum {
+		t.Errorf("%s: Checksums do not match", testName)
+	}
+}
+
+func TestEndToEnd(t *testing.T) {
+	// Startup test client with all dependencies and config
+	logger, err := logging.InitLogger()
+	if err != nil {
+		t.Fatalf("failed to create logger: %v", err)
+	}
+	client := NewTestClient(t, logger)
+	// defer client.Close()
+
+	// Test inputs
+	testFilesDir := utils.GetEnvString("TEST_FILES_DIR", "/app/test-files")
+	baseUploadAt := "/user/files"
+	defaultChunkSize := config.DefaultStreamerConfig(true).ChunkStreamSize
+
+	// Wait for datanodes states to be known by all datanodes -- heartbeat every 2s
+	// Temporary solution until service discovery is implemented
+	time.Sleep(4 * time.Second)
+
+	// Test varying file sizes
+	fileSizeTestCases := []struct {
+		name      string
+		filename  string
+		chunkSize int
+	}{
+		{name: "structured file test - 1000 lines", filename: "small_structured_test.txt", chunkSize: defaultChunkSize},
+		{name: "structured file test - 50000 lines", filename: "structured_test.txt", chunkSize: defaultChunkSize},
+		{name: "file size tests - 1MB", filename: "small_test.txt", chunkSize: defaultChunkSize},
+		{name: "file size tests - 10MB", filename: "medium_test.txt", chunkSize: defaultChunkSize},
+		{name: "file size tests - 100MB", filename: "large_test.txt", chunkSize: defaultChunkSize},
+		{name: "chunk size tests - 1MB", filename: "small_test.txt", chunkSize: 1 * 1024 * 1024},
+		{name: "chunk size tests - 4MB", filename: "medium_test.txt", chunkSize: 4 * 1024 * 1024},
+		{name: "chunk size tests - 8MB", filename: "large_test.txt", chunkSize: 8 * 1024 * 1024},
+		{name: "chunk size tests - 32MB", filename: "large_test.txt", chunkSize: 32 * 1024 * 1024},
+		{name: "chunk size tests - 64MB", filename: "large_test.txt", chunkSize: 64 * 1024 * 1024},
+	}
+
+	for _, tt := range fileSizeTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			t.Logf("Running test case: %s", tt.name)
+
+			testFilePath := filepath.Join(testFilesDir, tt.filename)
+			file, err := os.Open(testFilePath)
+			if err != nil {
+				t.Fatalf("%s: failed to open file: %v", tt.name, err)
+			}
+			defer file.Close()
+
+			uploadAt := filepath.Join(baseUploadAt, strconv.Itoa(rand.Intn(1000000)))
+			t.Logf("%s: Uploading file: %s", tt.name, tt.filename)
+			chunkInfos, err := client.UploadFile(file, uploadAt, tt.chunkSize)
+			if err != nil {
+				t.Errorf("%s: failed to upload file: %v", tt.name, err)
+			}
+			t.Logf("%s: Uploaded file: %s", tt.name, tt.filename)
+
+			// First, try downloading the file from the coordinator
+			fullFilepath := filepath.Join(uploadAt, tt.filename)
+			downloadFilePath, err := client.DownloadFile(fullFilepath)
+			if err != nil {
+				t.Fatalf("%s: failed to download file: %v", tt.name, err)
+			}
+			t.Logf("%s: Downloaded file: %s", tt.name, downloadFilePath)
+
+			// Check the integrity of the downloaded file
+			compareChecksums(t, testFilePath, downloadFilePath, tt.name)
+
+			// Download all chunks from all replicas for this file and validate the checksum
+			// bypasses the coordinator and downloads the file directly from the datanodes
+			// so that we can check if all replicas have all chunks they report to have
+			t.Logf("Downloading all chunks from all replicas for file: %s", tt.filename)
+			if err := client.DownloadAllChunks(t, chunkInfos, logger); err != nil {
+				t.Errorf("%s: failed to download file: %v", tt.name, err)
+			}
+
+			// Delete the file
+			t.Logf("Deleting file: %s", fullFilepath)
+			if err := client.DeleteFile(fullFilepath); err != nil {
+				t.Errorf("failed to delete file: %v", err)
+			}
+
+			// Try to download the file again, we expect to receive an error
+			// this method calls the coordinator to download the file.
+			t.Logf("Trying to download deleted file: %s", fullFilepath)
+			if _, err := client.DownloadFile(fullFilepath); err == nil {
+				t.Errorf("expected error when downloading deleted file")
+			}
+		})
+	}
+}
---
+++ dfs/tests/e2e/test_client.go
@@ -0,0 +1,171 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"sync"
+	"testing"
+
+	"distributed-file-system/internal/client"
+	"distributed-file-system/internal/client/downloader"
+	"distributed-file-system/internal/client/uploader"
+	"distributed-file-system/internal/clients"
+	"distributed-file-system/internal/common"
+	"distributed-file-system/internal/config"
+	"distributed-file-system/internal/storage/chunk"
+	"distributed-file-system/pkg/streaming"
+	"distributed-file-system/pkg/utils"
+)
+
+type datanodeClientPool struct {
+	clients map[string]*clients.DataNodeClient
+	mu      sync.Mutex
+}
+
+func (p *datanodeClientPool) getOrAddClient(replica *common.NodeInfo) (*clients.DataNodeClient, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	dnClient, ok := p.clients[replica.ID]
+	if !ok {
+		var err error
+		dnClient, err = clients.NewDataNodeClient(replica)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create datanode client: %w", err)
+		}
+		p.clients[replica.ID] = dnClient
+	}
+
+	return dnClient, nil
+}
+
+func (p *datanodeClientPool) closeAll() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	for _, client := range p.clients {
+		client.Close()
+	}
+}
+
+// Global datanode client pool
+var dnClientPool = &datanodeClientPool{
+	mu:      sync.Mutex{},
+	clients: make(map[string]*clients.DataNodeClient),
+}
+
+// Wrapper around the client to provide some test utilities
+type TestClient struct {
+	client *client.Client
+}
+
+func NewTestClient(t *testing.T, logger *slog.Logger) *TestClient {
+	coordinatorHost := utils.GetEnvString("COORDINATOR_HOST", "coordinator")
+	coordinatorPort := utils.GetEnvInt("COORDINATOR_PORT", 8080)
+	coordinatorNode := &common.NodeInfo{
+		ID:     "coordinator",
+		Host:   coordinatorHost,
+		Port:   coordinatorPort,
+		Status: common.NodeHealthy,
+	}
+	coordinatorClient, err := clients.NewCoordinatorClient(coordinatorNode)
+	if err != nil {
+		t.Fatalf("failed to create coordinator client: %v", err)
+	}
+
+	streamer := streaming.NewClientStreamer(config.DefaultStreamerConfig(true))
+	uploader := uploader.NewUploader(streamer, logger, uploader.UploaderConfig{
+		NumWorkers:      16,
+		ChunkRetryCount: 3,
+	})
+	downloader := downloader.NewDownloader(streamer, downloader.DownloaderConfig{
+		NumWorkers:      16,
+		ChunkRetryCount: 3,
+		TempDir:         "/tmp",
+	})
+
+	return &TestClient{
+		client: client.NewClient(coordinatorClient, uploader, downloader, streamer, logger),
+	}
+}
+
+func (c *TestClient) UploadFile(file *os.File, uploadAt string, chunkSize int) ([]common.ChunkInfo, error) {
+	chunkInfos, err := c.client.UploadFile(context.Background(), file, uploadAt, chunkSize)
+	if err != nil {
+		return nil, err
+	}
+	return chunkInfos, nil
+}
+
+func (c *TestClient) DownloadFile(path string) (string, error) {
+	return c.client.DownloadFile(context.Background(), path)
+}
+
+func (c *TestClient) DownloadAllChunks(t *testing.T, chunkInfos []common.ChunkInfo, logger *slog.Logger) error {
+	// Validate if the chunks were replicated to the provided nodes and their checksum matches the expectation
+	streamerInstance := streaming.NewClientStreamer(config.StreamerConfig{
+		ChunkStreamSize: config.DefaultStreamerConfig(true).ChunkStreamSize,
+	})
+	streamerInstance.Config().WaitReplicas = true // Wait for the final stream with the replicas information
+
+	for _, info := range chunkInfos {
+		for _, replica := range info.Replicas {
+			if err := downloadChunk(t, info, replica, streamerInstance, logger); err != nil {
+				return err
+			}
+		}
+	}
+
+	// dnClientPool.closeAll()
+
+	return nil
+}
+
+func downloadChunk(t *testing.T, info common.ChunkInfo, replica *common.NodeInfo, streamerInstance streaming.ClientStreamer, logger *slog.Logger) error {
+	dnClient, err := dnClientPool.getOrAddClient(replica)
+	if err != nil {
+		t.Errorf("failed to get datanode client: %v", err)
+	}
+
+	resp, err := dnClient.PrepareChunkDownload(context.Background(), common.DownloadChunkRequest{ChunkID: info.Header.ID})
+	if err != nil {
+		t.Errorf("retrieve %s: %v", info.Header.ID, err)
+	}
+
+	if !resp.Accept {
+		t.Errorf("node %s did not accept chunk %s download", replica.ID, info.Header.ID)
+	}
+
+	stream, err := dnClient.DownloadChunkStream(context.Background(), common.DownloadStreamRequest{
+		SessionID:       resp.SessionID,
+		ChunkStreamSize: config.DefaultStreamerConfig(true).ChunkStreamSize, // Follows the default chunk stream size -- 256KB
+	})
+	if err != nil {
+		t.Fatalf("failed to create download stream: %v", err)
+	}
+
+	buffer := bytes.NewBuffer(make([]byte, 0, info.Header.Size))
+
+	if err := streamerInstance.ReceiveChunkStream(context.Background(), stream, buffer, logger, streaming.DownloadChunkStreamParams{
+		SessionID:   resp.SessionID,
+		ChunkHeader: info.Header,
+	}); err != nil {
+		t.Errorf("failed to receive chunk stream: %v", err)
+	}
+
+	if checksum := chunk.CalculateChecksum(buffer.Bytes()); checksum != info.Header.Checksum {
+		t.Errorf("checksum mismatch for %s", info.Header.ID)
+	}
+
+	return nil
+}
+
+func (c *TestClient) DeleteFile(path string) error {
+	return c.client.DeleteFile(context.Background(), path)
+}
+
+func (c *TestClient) Close() error {
+	return c.client.Close()
+}
